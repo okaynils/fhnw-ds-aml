3539d3362be650d6202da5fd822c057a9b6efada
Nils <nils.fahrni@students.fhnw.ch>
2024-06-12 17:54:36 +0200

    parallelized cross_validate function

--- src/train_utils.py
+++ src/train_utils.py
@@ -7,6 +7,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_selection import RFE
 from tqdm import tqdm
+from joblib import Parallel, delayed
 from sklearn.model_selection import train_test_split
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.model_selection import StratifiedKFold, GridSearchCV
@@ -75,6 +76,35 @@ def build_preprocessor_pipeline(X_train, include_columns=None, regex_columns=Non
     return preprocessor, column_selection_train
 
 
+def process_fold(train_index, test_index, X, y, best_estimator, param_grid, grid_search, random_state):
+    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+    
+    estimator = best_estimator
+    if param_grid is not None:
+        estimator = grid_search.best_estimator_
+
+    if hasattr(estimator.named_steps['classifier'], 'random_state'):
+        estimator.named_steps['classifier'].random_state = random_state
+
+    estimator.fit(X_train, y_train)
+    
+    y_pred = estimator.predict(X_test)
+    y_prob = estimator.predict_proba(X_test)[:, 1]
+    
+    metrics = {
+        'Accuracy': accuracy_score(y_test, y_pred),
+        'Precision': precision_score(y_test, y_pred, average='weighted'),
+        'Recall': recall_score(y_test, y_pred, average='weighted'),
+        'F1 Score': f1_score(y_test, y_pred, average='weighted'),
+        'ROC AUC': roc_auc_score(y_test, y_prob)
+    }
+
+    fpr, tpr, _ = roc_curve(y_test, y_prob)
+    precision, recall, _ = precision_recall_curve(y_test, y_prob)
+
+    return metrics, (fpr, tpr), (precision, recall), y_prob, y_test
+
 def cross_validate(pipeline, X, y, n_splits=5, param_grid=None, random_state=1337, n_features_to_select=None):
     skf = StratifiedKFold(n_splits=n_splits, shuffle=False)
     
@@ -93,44 +123,25 @@ def cross_validate(pipeline, X, y, n_splits=5, param_grid=None, random_state=133
         grid_search = GridSearchCV(pipeline, param_grid, cv=skf, n_jobs=-1)
         grid_search.fit(X, y)
         best_params = grid_search.best_params_
+    else:
+        grid_search = None
     
-    metrics_list = []
-    roc_curves = []
-    pr_curves = []
-    lift_probs = []
-    true_labels = []
-
-    for train_index, test_index in tqdm(skf.split(X, y), total=skf.get_n_splits(), desc="Cross-Validation"):
-        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-        
-        if param_grid is not None:
-            best_estimator = grid_search.best_estimator_
-
-        if hasattr(best_estimator.named_steps['classifier'], 'random_state'):
-            best_estimator.named_steps['classifier'].random_state = random_state
-
-        best_estimator.fit(X_train, y_train)
-        
-        y_pred = best_estimator.predict(X_test)
-        y_prob = best_estimator.predict_proba(X_test)[:, 1]
-        
-        metrics_list.append({
-            'Accuracy': accuracy_score(y_test, y_pred),
-            'Precision': precision_score(y_test, y_pred, average='weighted'),
-            'Recall': recall_score(y_test, y_pred, average='weighted'),
-            'F1 Score': f1_score(y_test, y_pred, average='weighted'),
-            'ROC AUC': roc_auc_score(y_test, y_prob)
-        })
-
-        fpr, tpr, _ = roc_curve(y_test, y_prob)
-        precision, recall, _ = precision_recall_curve(y_test, y_prob)
-
-        roc_curves.append((fpr, tpr))
-        pr_curves.append((precision, recall))
-        lift_probs.append(y_prob)
-        true_labels.append(y_test)
+    results = Parallel(n_jobs=-1)(
+        delayed(process_fold)(train_index, test_index, X, y, best_estimator, param_grid, grid_search, random_state) 
+        for train_index, test_index in tqdm(skf.split(X, y), total=skf.get_n_splits(), desc="Cross-Validation")
+    )
+    
+    metrics_list, roc_curves, pr_curves, lift_probs, true_labels = zip(*results)
     
     metrics_df = pd.DataFrame(metrics_list)
     
+    # Fit the best estimator on the whole dataset
+    if param_grid is not None:
+        best_estimator = grid_search.best_estimator_
+
+    if hasattr(best_estimator.named_steps['classifier'], 'random_state'):
+        best_estimator.named_steps['classifier'].random_state = random_state
+
+    best_estimator.fit(X, y)
+
     return best_estimator, best_params, metrics_df, roc_curves, pr_curves, lift_probs, true_labels
